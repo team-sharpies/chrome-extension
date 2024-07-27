e752ba57eeca146a6eb15379b880665dbba0e13c
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2024-07-27 14:41:16 +1200

    wip, setting up hmr with vite

---
+++ src/sidepane/sidepane.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+const Sidepane: React.FC = () => {
+  return (
+    <div className="sidepane">
+      <h1>Sidepane Extension</h1>
+      <p>hyyyyy</p>
+    </div>
+  )
+}
+
+createRoot(document.getElementById('root')!).render(<Sidepane />)
