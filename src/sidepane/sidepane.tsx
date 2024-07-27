a282514925d0de13181455b1ec4e6c263ab7b0ef
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-07-28 11:31:55 +1200

    add new button title

--- src/sidepane/sidepane.tsx
+++ src/sidepane/sidepane.tsx
@@ -152,7 +152,7 @@ const Sidepane: React.FC = () => {
           checked={quizModeOn}
         />
         <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-cyan"></div>
-        <span className="ms-3 text-sm font-medium text-black">Quiz Mode</span>
+        <span className="ms-3 text-sm font-medium text-black">Quiz me 💡</span>
       </label>
 
       {quizModeOn ? (
